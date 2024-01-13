977283c5ea3c6bdedb790ec4bb2bef492554b47e
celestialmk <martinsemakula1000@gmail.com>
2024-01-13 20:35:18 +0300

    show model parameters

--- BuildNeuralNetwork/build_nn.py
+++ BuildNeuralNetwork/build_nn.py
@@ -33,3 +33,8 @@ class NeuralNetwork(nn.Module):
     
 model = NeuralNetwork().to(device)
 print(model)
+
+print(f"Model structure: {model}\n\n")
+
+for name, param in model.named_parameters():
+    print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
